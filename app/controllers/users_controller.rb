d79045e9e4e569dac5a0af85d63a20e06698a1cc
jeehyekim <iam@jeehyekim.com>
2016-02-11 12:48:16 -0800

    create users model and controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,15 @@
+class UsersController < ApplicationController
+  def new
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  private
+  	def user_params
+  		params.require(:user).permit(:email, :password)
+  	end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
 
+  get 'users/new'
+
+  get 'users/create'
+
+  get 'users/show'
+
   get 'static_pages/home'
 
   get 'static_pages/about'
