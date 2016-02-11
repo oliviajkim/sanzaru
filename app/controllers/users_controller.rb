d21c1a7e38f9fc54592e290b47481c13c0a7251c
jeehyekim <iam@jeehyekim.com>
2016-02-11 14:04:02 -0800

    styling for user new page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,18 @@
 class UsersController < ApplicationController
   def new
+  	@user = User.new
+  	render :new
   end
 
   def create
+  	@user = User.new(user_params)
+
+  	if @user.save
+  		session[:user_id] = @user.id
+  		redirect_to @user
+  	else
+  		render :new
+  	end
   end
 
   def show
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
 class User < ActiveRecord::Base
+	# require 'bcrypt'
 	has_secure_password
 end
