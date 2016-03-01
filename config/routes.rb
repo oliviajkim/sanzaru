0c5695486710cfe40aca835efe470bf9cc44c208
jeehyekim <iam@jeehyekim.com>
2016-02-29 22:40:33 -0800

    revising user new route

--- config/routes.rb
+++ config/routes.rb
@@ -22,6 +22,8 @@ Rails.application.routes.draw do
 
   resources :users
 
+  get '/sz/account/new', to: 'users#new'
+
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete "logout", to: "sessions#destroy"
